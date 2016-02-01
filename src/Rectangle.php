ed09eecb89ab0885724d85e2994bf2b2ae00a0b0
kmcmurtrey <korey.mcmurtrey@gmail.com>
2016-01-31 20:00:13 -0500

    Adds requirements for rectangle class

---
+++ src/Rectangle.php
@@ -0,0 +1,7 @@
+<?php
+
+class Rectangle extends Shape {
+    const SHAPE_TYPE = 2;
+
+//Rectangle class should not require any additional properties or methods, as it inherits them from Shape class.
+}
