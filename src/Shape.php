adc8324d44f593a39cff420223006ef63de15ab7
kmcmurtrey <korey.mcmurtrey@gmail.com>
2016-01-31 19:58:08 -0500

    Adds methods and properties for shape class

--- src/Shape.php
+++ src/Shape.php
@@ -38,9 +38,23 @@ Area method to calculate and return the area of the Shape object
     public function area($length, $width) {
         $this->length = $length;
         $this->width = $width;
-        
+
         $area = $length x $width;
         return $area;
+    }
+
+/*
+getTypeDexcription method to return the shape type
+*/
+    public static function getTypeDescription() {
+        return 'Type: ' . self::SHAPE_TYPE;
+    }
+
+/*
+getFullDescription method to return string describing the shape
+*/
+    public getFullDescription() {
+        return 'Shape' . $this->id . ': ' . $this->name . ' - ' . $this->length . ' x ' . $this->width;
     }
 
 }
