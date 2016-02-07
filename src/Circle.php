c84002012845e8622325a0db8ea9416ebc7fc043
Calvin Miracle <cbmira01@gmail.com>
2016-02-06 21:24:12 -0500

    Update Circle.php

--- src/Circle.php
+++ src/Circle.php
@@ -1,28 +1,26 @@
 <?php
 
-  require_once("Shape.php");
-  
   class Circle extends Shape {
     
     // class constants
     const SHAPE_TYPE = 3;
     
     // property declarations
+    private $id;
     protected $radius;
 
     // method declarations
     public function __construct( $radius ) {      
       parent::__construct();
+      $this->radius = $radius;
+      $this->id = uniqid();
     }
     
     public function area() {
       return M_PI * $this->radius * $this->radius;
     }
-     
-    public function getFullDescription() {
-      return "Circle<#" . $this->id . ">: " . $this->name . " - " . $this->radius;
-    }
-      
+          
   } // end class
 
 ?>
+
