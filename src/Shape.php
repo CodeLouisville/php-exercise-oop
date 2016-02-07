1f6bd9547d80f8631fcc38f09381c8654ec725f6
Calvin Miracle <cbmira01@gmail.com>
2016-02-06 19:38:28 -0500

    Update Shape.php

--- src/Shape.php
+++ src/Shape.php
@@ -1,27 +1,47 @@
 <?php
 
-  require_once("Shape.php");
-  
-  class Circle extends Shape {
+  class Shape {
     
     // class constants
-    const SHAPE_TYPE = 3;
+    const SHAPE_TYPE = 1;
     
     // property declarations
-    protected $radius;
-
-    // method declarations
-    function __construct( $radius ) {      
-      parent::__construct();
+    public $name;
+    protected $length;
+    protected $width;
+    private $id;
+     
+    // method declarations   
+    void __construct ( $length = 0, $width = 0 ) {
+      $this->length = $length;
+      $this->width = $width;      
+      $this->id = uniqid();
+    }
+    
+    public function getName() {
+      return $this->name;
+    }
+    
+    public function setName( $name ) {
+      $this->name = $name;
+    }
+    
+    public function getId() {
+      return $this->id;
     }
     
     public function area() {
-      return M_PI * $this->radius * $this->radius;
+      return $this->length * $this->width;
     }
-     
-    public getFullDescription() {
-      return "Circle<#" . $this->id . ">: " . $this->name . " - " . $this->radius;
+    
+    static public function getTypeDescription() {
+      return "Type: " . self::SHAPE_TYPE;
     }
+    
+    public function getFullDescription() {
+      return "Shape<#" . $this->id . ">: " . $this->name . " - " . $this->length . " x " . $this->width;
+    }
+    
       
   } // end class
 
