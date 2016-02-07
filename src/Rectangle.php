809d9e4bb1d3878b486cfdcd4ca1035b40f19e93
Calvin Miracle <cbmira01@gmail.com>
2016-02-06 21:24:42 -0500

    Update Rectangle.php

--- src/Rectangle.php
+++ src/Rectangle.php
@@ -1,12 +1,11 @@
 <?php
 
-  require_once("Shape.php");
-
   class Rectangle extends Shape {
     
     // class constants
-    const SHAPE_TYPE = 2;
+    const SHAPE_TYPE = 2;    
       
   } // end class
 
 ?>
+
