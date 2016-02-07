55a829fc03ba9e2213f6acdc88a257a3cdfe6563
Calvin Miracle <cbmira01@gmail.com>
2016-02-06 19:36:09 -0500

    Create Shape.php

---
+++ src/Shape.php
@@ -0,0 +1,28 @@
+<?php
+
+  require_once("Shape.php");
+  
+  class Circle extends Shape {
+    
+    // class constants
+    const SHAPE_TYPE = 3;
+    
+    // property declarations
+    protected $radius;
+
+    // method declarations
+    function __construct( $radius ) {      
+      parent::__construct();
+    }
+    
+    public function area() {
+      return M_PI * $this->radius * $this->radius;
+    }
+     
+    public getFullDescription() {
+      return "Circle<#" . $this->id . ">: " . $this->name . " - " . $this->radius;
+    }
+      
+  } // end class
+
+?>
